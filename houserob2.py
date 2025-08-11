cef83ef006162c42cfd071514956b0e9dada623b
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-11 08:09:24 -0500

    Update houserob2.py

--- houserob2.py
+++ houserob2.py
@@ -8,3 +8,14 @@ class Solution(object):
         """
         if len(nums) == 1:
             return nums[0]
+
+        def helperRob(subNum):
+            prev = 0
+            prevnot = 0
+            for num in subNum:
+                temp = max(prev, num + prevnot)
+                prevnot = prev
+                prev = temp
+            return prev
+
+        return max(helperRob(nums[1:]), helperRob(nums[:-1]) )
